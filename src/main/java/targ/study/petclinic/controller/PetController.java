1c3a724f7f3019990896f9c5575632a1ba2561c3
Emanuel Targino <emanueltargino1@gmail.com>
2022-03-21 13:50:26 -0300

    feature(backend): remover pet pelo id

--- src/main/java/targ/study/petclinic/controller/PetController.java
+++ src/main/java/targ/study/petclinic/controller/PetController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import targ.study.petclinic.model.DTO.ResponseDTO;
 import targ.study.petclinic.model.Pet;
 import targ.study.petclinic.service.PetService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/pet")
@@ -35,5 +38,11 @@ public class PetController {
     @GetMapping(value = "/buscar-por-id/{id}")
     public ResponseEntity<?> buscarPorId(@PathVariable Integer id){
         return new ResponseEntity<>(petService.buscarPorId(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/remover/{id}")
+    public ResponseEntity<ResponseDTO<Pet>> remover(@PathVariable Integer id){
+        petService.remover(id);
+        return new ResponseEntity<>(new ResponseDTO<>(200, List.of("Pet deletado!"), null), HttpStatus.OK);
     }
 }
