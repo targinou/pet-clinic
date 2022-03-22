df53401a080734d59dadaa628c2e508cc17711b2
Emanuel Targino <emanueltargino1@gmail.com>
2022-03-22 13:50:35 -0300

    feature(backend): adicionando métodos de para cadastrar consulta

--- src/main/java/targ/study/petclinic/controllers/PetController.java
+++ src/main/java/targ/study/petclinic/controllers/PetController.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import targ.study.petclinic.models.Consulta;
 import targ.study.petclinic.models.DTO.ResponseDTO;
 import targ.study.petclinic.models.Pet;
+import targ.study.petclinic.models.StatusConsulta;
+import targ.study.petclinic.models.Vet;
+import targ.study.petclinic.services.ConsultaService;
 import targ.study.petclinic.services.PetService;
+import targ.study.petclinic.services.VetService;
 
 import java.util.List;
 
@@ -17,8 +22,16 @@ public class PetController {
     @Autowired
     private final PetService petService;
 
-    public PetController(PetService petService) {
+    @Autowired
+    private final ConsultaService consultaService;
+
+    @Autowired
+    private final VetService vetService;
+
+    public PetController(PetService petService, ConsultaService consultaService, VetService vetService) {
         this.petService = petService;
+        this.consultaService = consultaService;
+        this.vetService = vetService;
     }
 
     @PostMapping(value = "/cadastrar")
@@ -53,5 +66,22 @@ public class PetController {
             return new ResponseEntity<>(new ResponseDTO<>(200, List.of("Pet atualizado!"), null), HttpStatus.OK);
         }
         else return null;
+    }
+
+    @PostMapping(value = "/cadastrar-consulta/{idPet}/vet/{idVet}")
+    public ResponseEntity<?> cadastrarPet(@PathVariable Integer idPet,@PathVariable Integer idVet, @RequestBody Consulta consulta){
+        Pet petSalvo = petService.buscarPorId(idPet);
+        Vet vetSalvo = vetService.buscaPorId(idVet);
+
+        consulta.setPet(petSalvo);
+        consulta.setVet(vetSalvo);
+        consulta.setStatus(StatusConsulta.EmAnalise);
+
+        Consulta consultaSalva = consultaService.cadastrar(consulta);
+        List<Consulta> ListaConsultasPet = petSalvo.getConsultas();
+        ListaConsultasPet.add(consultaSalva);
+        petSalvo.setConsultas(ListaConsultasPet);
+        this.petService.cadastrar(petSalvo);
+        return new ResponseEntity<>(consultaSalva, HttpStatus.OK);
     }
 }
