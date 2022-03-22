1e91adc8319020c31eb47984df6922df2be94be7
Emanuel Targino <emanueltargino1@gmail.com>
2022-03-22 13:49:42 -0300

    feature(backend): adicionando StatusConsulta

---
+++ src/main/java/targ/study/petclinic/models/StatusConsulta.java
@@ -0,0 +1,10 @@
+package targ.study.petclinic.models;
+
+
+public enum StatusConsulta {EmAnalise("Em analise"), Confirmado("Confirmado"), Rejeitado("Rejeitado");
+
+    StatusConsulta(String i) {
+
+    }
+}
+
