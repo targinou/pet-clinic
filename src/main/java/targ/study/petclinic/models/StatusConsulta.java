66341bc3c32f076c5abee515659d57fa843345c8
Emanuel Targino <emanueltargino1@gmail.com>
2022-03-23 12:45:01 -0300

    fix(backend): Ajuste Enum StatusConsulta

--- src/main/java/targ/study/petclinic/models/StatusConsulta.java
+++ src/main/java/targ/study/petclinic/models/StatusConsulta.java
@@ -1,10 +1,39 @@
 package targ.study.petclinic.models;
 
 
-public enum StatusConsulta {EmAnalise("Em analise"), Confirmado("Confirmado"), Rejeitado("Rejeitado");
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
-    StatusConsulta(String i) {
+@Getter
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum StatusConsulta {
+    EmAnalise("Em analise"),
+    Confirmado("Confirmado"),
+    Rejeitado("Rejeitado");
 
+    private final String codigo;
+    private final String descricao;
+
+
+    StatusConsulta(String descricao) {
+        this.codigo = this.name();
+        this.descricao = descricao;
+    }
+
+    @JsonCreator
+    public static StatusConsulta deserializar(@JsonProperty("codigo") String codigo){
+        return StatusConsulta.valueOf(codigo);
+    }
+
+    public static StatusConsulta fromString(String from){
+        for (StatusConsulta statusConsulta: StatusConsulta.values()){
+           if(statusConsulta.descricao.equals(from)){
+               return statusConsulta;
+           }
+        }
+        return null;
     }
 }
 
